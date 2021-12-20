efc4cc48ed94fbc6799e00377b3a7249a54adc66
kevren22 <kevren22@gmail.com>
2021-12-19 22:24:18 -0500

    set up initial player buttons and point values

---
+++ app.js
@@ -0,0 +1,18 @@
+const p1 = {
+	score: 0,
+	button: document.querySelector("#p1Button"),
+	display: document.querySelector("#p1Display"),
+};
+const p2 = {
+	score: 0,
+	button: document.querySelector("#p2Button"),
+	display: document.querySelector("#p2Display"),
+};
+
+const pointValues = {
+	0: 0,
+	1: 15,
+	2: 30,
+	3: 40,
+	4: "AD",
+};
