86c4ff81b2927dd9b28a81dad77720983f6bdd77
kevren22 <kevren22@gmail.com>
2022-01-26 20:33:55 -0500

    add game score logic, connect to sets

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const p1 = {
 	score: 0,
 	button: document.querySelector('#p1Button'),
-	display: document.querySelector('#p1Display'),
-	gameScore: document.querySelector('#p1GameScore'),
+	gameDisplay: document.querySelector('#p1GameScore'),
+	setScore: {
+		1: 0,
+		2: 0,
+		3: 0,
+	},
+	setDisplay: {
+		1: document.querySelector('#p1Set1Disp'),
+		2: document.querySelector('#p1Set2Disp'),
+		3: document.querySelector('#p1Set3Disp'),
+	},
 };
 const p2 = {
 	score: 0,
 	button: document.querySelector('#p2Button'),
-	display: document.querySelector('#p2Display'),
-	gameScore: document.querySelector('#p2GameScore'),
+	gameDisplay: document.querySelector('#p2GameScore'),
+	setScore: {
+		1: 0,
+		2: 0,
+		3: 0,
+	},
+	setDisplay: {
+		1: document.querySelector('#p2Set1Disp'),
+		2: document.querySelector('#p2Set2Disp'),
+		3: document.querySelector('#p2Set3Disp'),
+	},
 };
 
 const pointValues = {
@@ -19,14 +37,31 @@ const pointValues = {
 	4: 'AD',
 };
 
-function addPoint(player) {
+let isGameOver = false;
+
+let activeSet = 1;
+
+function addPoint(player, opp, currentSet) {
 	player.score += 1;
-	player.gameScore.innerText = pointValues[player.score];
+	if (player.score >= 4 && player.score - opp.score >= 2) {
+		player.setScore[currentSet] += 1;
+		player.score = 0;
+		opp.score = 0;
+		opp.gameDisplay.innerText = pointValues[opp.score];
+		player.setDisplay[currentSet].innerText = player.setScore[currentSet];
+	} else if (player.score === 4 && opp.score === 4) {
+		player.score = 3;
+		opp.score = 3;
+		opp.gameDisplay.innerText = pointValues[opp.score];
+	} else if (player.score === 4 && opp.score === 3) {
+		opp.gameDisplay.innerText = '';
+	}
+	player.gameDisplay.innerText = pointValues[player.score];
 }
 
 p1.button.addEventListener('click', () => {
-	addPoint(p1);
+	addPoint(p1, p2, activeSet);
 });
 p2.button.addEventListener('click', () => {
-	addPoint(p2);
+	addPoint(p2, p1, activeSet);
 });
